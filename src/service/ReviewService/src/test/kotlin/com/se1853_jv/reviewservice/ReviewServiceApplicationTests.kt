502060158abb204954cae7e8a505e6909be8f17f
TranHoan2004 <hoana5k44nknd@gmail.com>
2025-10-01 17:16:02 +0700

    fix: resolve conflicts after merging dev in the test package of Review Service

--- src/service/ReviewService/src/test/kotlin/com/se1853_jv/reviewservice/ReviewServiceApplicationTests.kt
+++ src/service/ReviewService/src/test/kotlin/com/se1853_jv/reviewservice/ReviewServiceApplicationTests.kt
@@ -1,18 +1,10 @@
-<<<<<<<< HEAD:src/service/BookService/src/test/kotlin/com/se1853_jv/bookservice/BookServiceApplicationTests.kt
-package com.se1853_jv.bookservice
-========
 package com.se1853_jv.reviewservice
->>>>>>>> 375e2e98b293b61d98a0e28f2636731a3d3dd943:src/service/ReviewService/src/test/kotlin/com/se1853_jv/reviewservice/ReviewServiceApplicationTests.kt
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-<<<<<<<< HEAD:src/service/BookService/src/test/kotlin/com/se1853_jv/bookservice/BookServiceApplicationTests.kt
-class BookServiceApplicationTests {
-========
 class ReviewServiceApplicationTests {
->>>>>>>> 375e2e98b293b61d98a0e28f2636731a3d3dd943:src/service/ReviewService/src/test/kotlin/com/se1853_jv/reviewservice/ReviewServiceApplicationTests.kt
 
     @Test
     fun contextLoads() {
